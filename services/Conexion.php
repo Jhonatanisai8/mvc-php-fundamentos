73160ed586868cfaa763e2e3e880ce9d489d7bec
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-11-14 20:01:44 -0500

    conexion con la base de datos

--- services/Conexion.php
+++ services/Conexion.php
@@ -17,9 +17,9 @@ class Conexion
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                 PDO::ATTR_EMULATE_PREPARES => false,
             ];
+            return new PDO($dsn, USUARIO, PASSWORD, $opciones);
         } catch (PDOException $e) {
             die("Error: " . $e->getMessage());
         }
-        return new PDO($dsn, USUARIO, PASSWORD, $opciones);
     }
 }
