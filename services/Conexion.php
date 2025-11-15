e02fd5d37e75fd1749122cc48e0003235f136d94
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-11-14 20:01:23 -0500

    conexion con la base de datos

---
+++ services/Conexion.php
@@ -0,0 +1,25 @@
+<?php
+
+class Conexion
+{
+    public static function conectarBD()
+    {
+        define("SERVIDOR", "localhost");
+        define("USUARIO", "root");
+        define("PASSWORD", "753159852");
+        define("DATABASE", "demo_labotario_11");
+        define("PORT", 3306);
+        define("CHARSET", "utf8");
+        $dsn = "mysql:host=SERVIDOR;dbname=DATABASE;charset=CHARSET";
+        try {
+            $opciones = [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES => false,
+            ];
+        } catch (PDOException $e) {
+            die("Error: " . $e->getMessage());
+        }
+        return new PDO($dsn, USUARIO, PASSWORD, $opciones);
+    }
+}
